87e2d542bd323175346128c4b652485c0306864d
Tanjim H. <contact@audacioustux.com>
2026-02-10 00:29:15 +0600

    feat: add musl targets for cross-compilation support

--- flake.nix
+++ flake.nix
@@ -39,6 +39,8 @@
           targets = [
             "x86_64-unknown-linux-gnu"
             "aarch64-unknown-linux-gnu"
+            "x86_64-unknown-linux-musl"
+            "aarch64-unknown-linux-musl"
           ];
         };
 
